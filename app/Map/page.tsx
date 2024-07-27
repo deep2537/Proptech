56a26920bbfa39c0333240a27f7a9c5bdad67f60
deep2537 <deepgada2003@gmail.com>
2024-07-27 18:14:05 +0530

    Deep work

--- app/Map/page.tsx
+++ app/Map/page.tsx
@@ -1,6 +1,5 @@
 "use client";
-import React from 'react';
-import { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 
@@ -9,43 +8,47 @@ const Page: React.FC = () => {
     const mapContainerRef = useRef<HTMLDivElement | null>(null);
 
     useEffect(() => {
-        if (mapContainerRef.current && !mapRef.current) {
-            const map = L.map(mapContainerRef.current, {
-                center: [19.076, 72.8777],
-                zoom: 15,
-                zoomControl: false,
-                crs: L.CRS.EPSG3857,
-                preferCanvas: false,
+        if (typeof window !== 'undefined') {
+            import('leaflet').then((L) => {
+                if (mapContainerRef.current && !mapRef.current) {
+                    const map = L.map(mapContainerRef.current, {
+                        center: [19.076, 72.8777],
+                        zoom: 15,
+                        zoomControl: false,
+                        crs: L.CRS.EPSG3857,
+                        preferCanvas: false,
+                    });
+
+                    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                        detectRetina: false,
+                        maxNativeZoom: 19,
+                        maxZoom: 19,
+                        minZoom: 0,
+                        noWrap: false,
+                        opacity: 1,
+                        tms: false,
+                    }).addTo(map);
+
+                    const selectedBounds = JSON.parse(localStorage.getItem('selectedBounds') || 'null');
+                    if (selectedBounds) {
+                        const bounds = L.latLngBounds(
+                            [selectedBounds[0][0], selectedBounds[0][1]],
+                            [selectedBounds[1][0], selectedBounds[1][1]]
+                        );
+
+                        const rectangle = L.rectangle(bounds, {
+                            color: 'blue',
+                            weight: 3,
+                            fillColor: 'green',
+                            fillOpacity: 0.0,
+                        }).addTo(map);
+
+                        map.fitBounds(rectangle.getBounds());
+                    }
+
+                    mapRef.current = map;
+                }
             });
-
-            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                detectRetina: false,
-                maxNativeZoom: 19,
-                maxZoom: 19,
-                minZoom: 0,
-                noWrap: false,
-                opacity: 1,
-                tms: false,
-            }).addTo(map);
-
-            const selectedBounds = JSON.parse(localStorage.getItem('selectedBounds') || 'null');
-            if (selectedBounds) {
-                const bounds = L.latLngBounds(
-                    [selectedBounds[0][0], selectedBounds[0][1]],
-                    [selectedBounds[1][0], selectedBounds[1][1]]
-                );
-
-                const rectangle = L.rectangle(bounds, {
-                    color: 'blue',
-                    weight: 3,
-                    fillColor: 'green',
-                    fillOpacity: 0.0,
-                }).addTo(map);
-
-                map.fitBounds(rectangle.getBounds());
-            }
-
-            mapRef.current = map;
         }
     }, []);
 
