640e4ba1e332fdcae24a7626e560e35e0548997c
deep2537 <deepgada2003@gmail.com>
2024-08-12 02:37:08 +0530

    First Iteration django backend

--- django backend/backend/backend/settings.py
+++ django backend/backend/backend/settings.py
@@ -1,4 +1,4 @@
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -107,7 +107,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
@@ -115,3 +115,4 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',  # Add your Next.js frontend URL
 ]
+ALLOWED_HOSTS = ['.vercel.app']
